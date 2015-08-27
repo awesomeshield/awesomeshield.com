0889a775d308d517c2ae32728c677ddd4d1cd7c2
Jon Amar <jon@solomongiles.com>
2015-08-27 19:33:14 +0200

    awesome shield v1

---
+++ js/index.js
@@ -0,0 +1,10 @@
+$('document').ready(function() {
+	var options = { 
+		videoId: '5jUpoAzs5ME', 
+		start: 0,
+		end: 10
+	};
+	$('#wrapper-hero').tubular(options);
+	// f-UGhWj1xww cool sepia hd
+	// 49SKbS7Xwf4 beautiful barn sepia
+});
